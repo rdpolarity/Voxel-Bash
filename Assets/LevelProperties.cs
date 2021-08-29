da58d14ce7a969cfc8be8ec49d3142917229a435
RDPolarity <aiden.mellor@greater.com.au>
2021-08-30 00:52:41 +1000

    Added new basic level

---
+++ Assets/LevelProperties.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelProperties : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> spawnLocations;
+
+    // A group of all objects to be destroyed overtime
+    [SerializeField]
+    private GameObject destructablesGroup;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
