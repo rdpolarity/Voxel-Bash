998ed732d5418213d7e6150e57132014be33bafa
RDPolarity <aiden.mellor@greater.com.au>
2021-09-02 16:12:20 +1000

    Organized assets, Added Mirror, Removed MLAPI, Added Steam Support

--- Assets/Scripts/InputControllers/PlayerController.cs
+++ Assets/Scripts/InputControllers/PlayerController.cs
@@ -2,18 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using MLAPI;
-using MLAPI.NetworkVariable;
-using MLAPI.Messaging;
 
-public class PlayerController : NetworkBehaviour
+public class PlayerController : MonoBehaviour
 {
-    public NetworkVariableVector3 Position = new NetworkVariableVector3(new NetworkVariableSettings
-    {
-        WritePermission = NetworkVariablePermission.ServerOnly,
-        ReadPermission = NetworkVariablePermission.Everyone
-    });
-
 
     [SerializeField]
     private PlayerInput inputs;
@@ -37,13 +28,6 @@ public class PlayerController : NetworkBehaviour
 
     private Vector3 mousePos = Vector3.zero;
 
-    [ServerRpc(RequireOwnership = false)]
-    void SubmitPositionRequestServerRpc(ServerRpcParams rpcParams = default)
-    {
-        Position.Value = gameObject.transform.position;
-    }
-
-
     private void Awake()
     {
         rigidbody = GetComponent<Rigidbody>();
@@ -52,62 +36,59 @@ public class PlayerController : NetworkBehaviour
 
     private void FixedUpdate()
     {
-        if (IsLocalPlayer) {            
-            velocity = new Vector3(movement.x * speed, 0, movement.y * speed);
-            rigidbody.AddForce(velocity, ForceMode.Impulse);
-            velocity = Vector3.zero;
+        velocity = new Vector3(movement.x * speed, 0, movement.y * speed);
+        rigidbody.AddForce(velocity, ForceMode.Impulse);
+        velocity = Vector3.zero;
 
-            // Falling Velocity
-            if (rigidbody.velocity.y < 0f)
-            {
-                rigidbody.velocity += Vector3.down * Physics.gravity.y * Time.fixedDeltaTime;
-            }
-
-            // Max Velocity
-            Vector3 horizontalVelocity = rigidbody.velocity;
-            horizontalVelocity.y = 0;
-            if (horizontalVelocity.sqrMagnitude > maxSpeed * maxSpeed)
-            {
-                rigidbody.velocity = horizontalVelocity.normalized * maxSpeed + Vector3.up * rigidbody.velocity.y;
-            }
-
-            // Look Direction (Y)
-            var ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
-            Physics.Raycast(ray, out var hit);
-            facing = Vector3.Normalize(hit.point - transform.position);
-            facing.y = 0;
-            var rotation = Quaternion.LookRotation(facing);
-            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * 10);
-            bow.Dir = facing;
-            SubmitPositionRequestServerRpc();
+        // Falling Velocity
+        if (rigidbody.velocity.y < 0f)
+        {
+            rigidbody.velocity += Vector3.down * Physics.gravity.y * Time.fixedDeltaTime;
         }
+
+        // Max Velocity
+        Vector3 horizontalVelocity = rigidbody.velocity;
+        horizontalVelocity.y = 0;
+        if (horizontalVelocity.sqrMagnitude > maxSpeed * maxSpeed)
+        {
+            rigidbody.velocity = horizontalVelocity.normalized * maxSpeed + Vector3.up * rigidbody.velocity.y;
+        }
+
+        // Look Direction (Y)
+        var ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+        Physics.Raycast(ray, out var hit);
+        facing = Vector3.Normalize(hit.point - transform.position);
+        facing.y = 0;
+        var rotation = Quaternion.LookRotation(facing);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * 10);
+        bow.Dir = facing;
     }
 
     public void OnMovement(InputAction.CallbackContext context)
     {
-        if (IsLocalPlayer) {              
-            if (context.performed)
-                animator.SetBool("isMoving", true);
-            if (context.canceled)
-                animator.SetBool("isMoving", false);
-            movement = context.ReadValue<Vector2>();
-        }
+
+        if (context.performed)
+            animator.SetBool("isMoving", true);
+        if (context.canceled)
+            animator.SetBool("isMoving", false);
+        movement = context.ReadValue<Vector2>();
+
     }
 
     public void OnShoot(InputAction.CallbackContext context)
     {
-        if (IsLocalPlayer) {         
-            if (context.started)
-            {
-                Debug.Log("Charging");
-                bow.Charging = true;
 
-            }
-            if (context.canceled)
-            {
-                Debug.Log("Release");
-                bow.Shoot(transform.position);
-            }
+        if (context.started)
+        {
+            Debug.Log("Charging");
+            bow.Charging = true;
+
         }
+        if (context.canceled)
+        {
+            Debug.Log("Release");
+            bow.Shoot(transform.position);
+        }
+
     }
 }
