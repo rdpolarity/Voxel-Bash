c00e693db7c783e17e8964e70f8b25ed05c86884
RDPolarity <rdpolarity@gmail.com>
2021-09-19 16:53:01 +1000

    Added Destroyed Block Effect

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using Mirror;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class Tile : MonoBehaviour
 {
     public GameObject nestedBlock;
-
+    public GameObject explosionEffect;
     public Vector2 coordinates;
     public List<Tile> neighbours;
     
     //IMPORTANT!!!! this method must be used to destroy a tile to prevent null fields in other lists
     public void Delete()
     {
+        Debug.Log("Destroyed");
+        if (explosionEffect != null) Instantiate(explosionEffect, transform.position, transform.rotation);
         foreach (Tile t in neighbours)
         {
             t.neighbours.Remove(this);
